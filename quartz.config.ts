3e39c24179b805877f5f8aa3fc7fc91f47014a43
Anders Kristian Munk <ankm@dtu.dk>
2026-03-04 00:08:33 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Anders Kristian Munk // Researcher",
+    pageTitle: "Anders Kristian Munk | Researcher",
     pageTitleSuffix: " | AK Munk",
     enableSPA: true,
     enablePopovers: true,
